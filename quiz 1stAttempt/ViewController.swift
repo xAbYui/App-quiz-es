b321aeaa6dbacf44731547b69810cfba3e59dd4e
渡辺唯都 <watanabeyuito@s-yuito-watanabe.local>
2017-06-11 10:51:48 +0900

    Initial Commit

---
+++ quiz 1stAttempt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quiz 1stAttempt
+//
+//  Created by 渡辺唯都 on 2017/06/11.
+//  Copyright © 2017年 渡辺唯都. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
