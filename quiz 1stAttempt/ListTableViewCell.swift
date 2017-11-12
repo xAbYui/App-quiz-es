d70ecc39bedbdba8b33e9e51611c2bff93a22685
渡辺唯都 <watanabeyuito@S-yuito-watanabe.local>
2017-11-12 12:09:32 +0900

    add gitignore

---
+++ quiz 1stAttempt/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  quiz 1stAttempt
+//
+//  Created by 渡辺唯都 on 2017/08/06.
+//  Copyright © 2017年 渡辺唯都. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ quiz 1stAttempt/QuizViewController.swift
@@ -0,0 +1,35 @@
+//
+//  QuizViewController.swift
+//  quiz 1stAttempt
+//
+//  Created by 渡辺唯都 on 2017/06/11.
+//  Copyright © 2017年 渡辺唯都. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
